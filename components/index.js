b68bfadf8ae70ead5ee87ae23ad05c64cfaae457
laihhi <mylaizhicheng@qq.com>
2017-09-12 15:53:00 +0800

    要提交的变更： 	修改：     components/index.js#	新文件：   public/images/logo.ico 	修改：     views/head.ejs 	修改：     views/index.ejs

--- components/index.js
+++ components/index.js
@@ -8,13 +8,7 @@ class Index extends React.Component {
   render(){
     return (
       <div>
-        <h1>Hello {window.ejsname} </h1>
-        <br />
-
-        <h3>{userFromServer.name}</h3>
-        <h3>{userFromServer.user}</h3>
-        <h3>{userFromServer.sex}</h3>
-        <h3>{userFromServer.age}</h3>
+        
         <Head name="React">
         </Head>
         <hr></hr>
